b9095bd0d10d395835bb190453900d4296c773d8
Yong <Yong@email>
2025-07-31 11:24:11 +0800

    fix

--- caiyicloud/serializers.py
+++ caiyicloud/serializers.py
@@ -4,8 +4,8 @@ from __future__ import unicode_literals
 import logging
 from rest_framework import serializers
 from django.utils import timezone
-
-from caiyicloud.models import CyTicketPack, CySession
+from restframework_ext.exceptions import CustomAPIException
+from caiyicloud.models import CyTicketPack, CyTicketType
 
 log = logging.getLogger(__name__)
 
@@ -17,7 +17,17 @@ class CyTicketPackSerializer(serializers.ModelSerializer):
 
 
 class CySeatUrlSerializer(serializers.ModelSerializer):
+    no = serializers.CharField(read_only=True)
+    navigate_url = serializers.CharField(read_only=True)
+
+    def create(self, validated_data):
+        try:
+            cf = CyTicketType.objects.get(cy_no=validated_data['no'])
+        except CyTicketType.DoesNotExist:
+            raise CustomAPIException('票档no错误')
+        ret = cf.cy_session.get_seat_url(validated_data['no'], validated_data['navigate_url'])
+        return ret
 
     class Meta:
-        model = CySession
-        fields = []
+        model = CyTicketType
+        fields = ['no', 'navigate_url']
