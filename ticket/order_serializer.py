4d5aa0ac5a8e0baf116708065b3321205b013247
Yong <Yong@email>
2025-08-02 10:33:38 +0800

    fix

--- ticket/order_serializer.py
+++ ticket/order_serializer.py
@@ -580,7 +580,7 @@ class CyTicketOrderCommonSerializer(TicketOrderCreateCommonSerializer):
         """
         # 彩艺云下单
         from caiyicloud.models import CyOrder
-        real_name_list = list(show_users.values('id_card', 'name')) if show_users else None
+        real_name_list = list(show_users.values('id_card', 'name', 'mobile')) if show_users else None
         # try:
         CyOrder.order_create(ticket_order=ticket_order, session=session, real_name_list=real_name_list,
                              seat_info=seat_info, ticket_list=ticket_list, amounts_data=amounts_data)
