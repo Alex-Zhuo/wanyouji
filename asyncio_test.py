ace88a06bfb0266cb1aea59709c729e4eb6d6202
Yong <Yong@email>
2025-08-15 17:09:20 +0800

    fix

---
+++ asyncio_test.py
@@ -0,0 +1,42 @@
+import aiohttp
+import asyncio
+import time
+import aioredis
+import contextlib
+
+
+@contextlib.asynccontextmanager
+async def get_pika_redis():
+    url = 'redis://172.16.0.10:9221/1'
+    redis = await aioredis.from_url(
+        url, decode_responses=True)
+    try:
+        yield redis
+    finally:
+        await redis.close()
+
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        start_time = time.time()
+        st = False
+        ret = await response.text()
+        status = response.status
+        if 200 <= status < 300:
+            st = True
+        end_time = time.time()
+        elapsed_ms = int((end_time - start_time) * 1000)  # 计算时间差ms
+        print('{},{}'.format(elapsed_ms, st))
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        redis = await get_pika_redis()
+        tasks = []
+        for i in list(range(0, 100)):
+            token = await redis.lindex('test_token', i)
+            tasks.append(fetch(session, f"http:127.0.0.1:8168/api/users/new_info/?Actoken={token}"))
+        # responses = await asyncio.gather(*tasks)
+
+
+asyncio.run(main())
