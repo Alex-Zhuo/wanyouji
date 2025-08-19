9ccebd79bf9daea7a28efba046d768c7fc73ef28
Yong <Yong@email>
2025-08-19 16:26:45 +0800

    fox

--- qcloud/__init__.py
+++ qcloud/__init__.py
@@ -103,9 +103,9 @@ class TencentCloudImpl(object):
             # sms helper: https://cloud.tencent.com/document/product/382/3773
 
             # 短信应用ID: 短信SdkAppid在 [短信控制台] 添加应用后生成的实际SdkAppid，示例如1400006666
-            req.SmsSdkAppid = self.appid
+            req.SmsSdkAppId = self.appid
             # 短信签名内容: 使用 UTF-8 编码，必须填写已审核通过的签名，签名信息可登录 [短信控制台] 查看
-            req.Sign = self.sign
+            req.SignName = self.sign
             # 短信码号扩展号: 默认未开通，如需开通请联系 [sms helper]
             req.ExtendCode = ""
             # 用户的 session 内容: 可以携带用户侧 ID 等上下文信息，server 会原样返回
@@ -127,7 +127,7 @@ class TencentCloudImpl(object):
                     data['name'] = data['name'].replace('【', '')
                     data['name'] = data['name'].replace('】', '')
                     name = '{}...'.format(data['name'][:10]) if len(data['name']) > 10 else data['name']
-                    template_param = [name, data['number'], data['time']]
+                    template_param = [name, str(data['number']), data['time']]
             else:
                 if data.get('code'):
                     template_code = self.template_login_code
@@ -137,13 +137,14 @@ class TencentCloudImpl(object):
                     data['name'] = data['name'].replace('【', '')
                     data['name'] = data['name'].replace('】', '')
                     name = '{}...'.format(data['name'][:10]) if len(data['name']) > 10 else data['name']
-                    template_param = [name, data['number'], data['time']]
+                    template_param = [name, str(data['number']), data['time']]
             # 模板 ID: 必须填写已审核通过的模板 ID。模板ID可登录 [短信控制台] 查看
-            req.TemplateID = template_code
+            req.TemplateId = template_code
             req.TemplateParamSet = template_param
 
             # 通过client对象调用DescribeInstances方法发起请求。注意请求方法名与请求对象是对应的。
             # 返回的resp是一个DescribeInstancesResponse类的实例，与请求对象对应。
+            print(req)
             logger.debug(req)
 
             def _send():
