28d1b3510a5b520afaeb64b248a6de586420b980
Yong <Yong@email>
2025-08-19 14:42:37 +0800

    fix

--- ai_agent/urls.py
+++ ai_agent/urls.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 from rest_framework.routers import DefaultRouter
 
-from ai_agent.views import DefaultQuestionsViewSet, HistoryChatViewSet, MoodImageViewSet
+from ai_agent.views import DefaultQuestionsViewSet, HistoryChatViewSet, MoodImageViewSet,ImageResourceViewSet
 
 router = DefaultRouter()
 
 router.register('questions', DefaultQuestionsViewSet)
 router.register('history', HistoryChatViewSet)
 router.register('mood', MoodImageViewSet)
-router.register('mood', MoodImageViewSet)
+router.register('resource', ImageResourceViewSet)
