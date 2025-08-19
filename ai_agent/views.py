b0da50c9d0da082b126a401761e7d4c7dc159e31
Yong <Yong@email>
2025-08-19 10:45:15 +0800

    fix

--- ai_agent/views.py
+++ ai_agent/views.py
@@ -24,9 +24,9 @@ class DefaultQuestionsViewSet(ReturnNoDetailViewSet):
     serializer_class = DefaultQuestionsSerializer
     http_method_names = ['get']
 
-    @method_decorator(cache_page(60, key_prefix=PREFIX))
+    # @method_decorator(cache_page(60, key_prefix=PREFIX))
     def list(self, request, *args, **kwargs):
-        return Response(self.serializer_class(self.queryset, context={'request': request}).data)
+        return Response(self.serializer_class(self.queryset, many=True, context={'request': request}).data)
 
     @action(methods=['post', 'get'], detail=False, http_method_names=['post', 'get'])
     def post_question(self, request):
