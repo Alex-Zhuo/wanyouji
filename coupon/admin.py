523eb38d2bdad8e88aff2f9ee1f7bbac3ae62041
Yong <Yong@email>
2025-08-22 18:23:19 +0800

    fix

--- coupon/admin.py
+++ coupon/admin.py
@@ -3,6 +3,23 @@ from django.contrib import admin
 from dj import technology_admin
 from coupon.models import UserCouponRecord, Coupon
 from dj_ext.permissions import RemoveDeleteModelAdmin, OnlyViewAdmin
+from django.contrib import messages
+
+
+def set_on(modeladmin, request, queryset):
+    queryset.update(status=Coupon.STATUS_ON)
+    messages.success(request, '执行成功')
+
+
+set_on.short_description = '批量上架'
+
+
+def set_off(modeladmin, request, queryset):
+    queryset.update(status=Coupon.STATUS_OFF)
+    messages.success(request, '执行成功')
+
+
+set_off.short_description = '批量下架'
 
 
 class CouponAdmin(RemoveDeleteModelAdmin):
@@ -11,6 +28,7 @@ class CouponAdmin(RemoveDeleteModelAdmin):
     list_filter = ['expire_time', 'limit_show_types_second', 'status']
     autocomplete_fields = ['shows', 'limit_show_types_second']
     readonly_fields = ['no']
+    actions = [set_on, set_off]
 
 
 class UserCouponRecordAdmin(OnlyViewAdmin):
