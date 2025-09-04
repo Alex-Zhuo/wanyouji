b5b9f9f00dbb16a8aa724728a29dd4b2254c8a37
Yong <Yong@email>
2025-09-04 12:23:16 +0800

    fix

--- caiyicloud/serializers.py
+++ caiyicloud/serializers.py
@@ -125,6 +125,7 @@ class CheckPromoteActivitySerializer(serializers.ModelSerializer):
 
     # @pysnooper.snoop(log.debug)
     def create(self, validated_data):
+        log.debug(validated_data)
         # ticket_list [dict(level_id=1,multiply=2)]
         event_qs, ticket_qs, session = PromoteActivity.get_promotes(validated_data['session_no'])
         # 优惠金额
