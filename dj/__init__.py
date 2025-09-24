c67b734f589fbd44f678d8f11c2e8e859455a861
Yong <Yong@email>
2025-09-24 11:39:47 +0800

    fix

--- dj/__init__.py
+++ dj/__init__.py
@@ -1,6 +1,6 @@
 from django.contrib.admin import AdminSite
 
-AdminSite.site_title = AdminSite.index_title = AdminSite.site_header = '谷得一下'
+AdminSite.site_title = AdminSite.index_title = AdminSite.site_header = '湾游记'
 technology_admin = AdminSite(name='technology_admin')
 from .celery import app as celery_app
 
