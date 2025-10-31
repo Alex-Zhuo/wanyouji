be7ba621e30b83d29262c9a3038320e3ce28d7fc
Yong <Yong@email>
2025-10-31 18:08:39 +0800

    fix

--- coupon/urls.py
+++ coupon/urls.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from coupon.views import CouponViewSet, UserCouponRecordViewSet, CouponReceiptViewSet
+from coupon.views import CouponViewSet, UserCouponRecordViewSet, CouponReceiptViewSet, CouponOrderViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -7,3 +7,4 @@ router = DefaultRouter()
 router.register(r'record', CouponViewSet)
 router.register(r'user', UserCouponRecordViewSet)
 router.register(r'receipt', CouponReceiptViewSet, basename='coupon_receipt')
+router.register(r'order', CouponOrderViewSet, basename='coupon_order')
